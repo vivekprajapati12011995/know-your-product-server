6ebafa179fce83a7ee5eff036912d01076a473bd
vprajapati <vickyprajpati244@yahoo.com>
2017-06-13 18:06:17 +0530

    server side coding started with demo service creating

--- server.js
+++ server.js
@@ -1,74 +1,23 @@
-const express = require('express');
-const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const ejsLint = require('ejs-lint');
-const url = require('url');
-const ObjectId = require('mongodb').ObjectID;
-const app = express();
-var fs = require("fs");
-const port = 3000;
-var db;
-app.set('view engine', 'ejs');
+var express = require('express');
 
+// Create our Express application
+var app = express();
 
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
 
-MongoClient.connect('mongodb://vivek_mongo:vivek1200@ds139761.mlab.com:39761/star_wars_db', (err, database) => {
-  if (err) return console.log(err);
-  db = database;
-  app.listen(port, () => {
-  console.log('listening on port: '+port);
-  })
-})
+// Create our Express router
+var router = express.Router();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+  res.json({ message: 'You are running dangerously low on beer!' });
+});
 
-    app.get('/user', (req, res) => {
-      db.collection('user').find().toArray(function(err, results) {
-        console.log(results);
-        res.end(JSON.stringify(results));
-      })
-    })
+// Register all our routes with /api
+app.use('/api', router);
 
-    app.get('/edit', (req, res) => {
-      var url_parts = url.parse(req.url, true);
-      var query = url_parts.query;
-      var _id = query._id;
-      db.collection('user').find({"_id": ObjectId(_id)}).toArray(function(err, results) {
-        console.log(JSON.stringify(results));
-        res.end(JSON.stringify(results));
-      })
-    })
-
-    app.post('/user', (req, res) => {
-      db.collection('user').save(req.body, (err, result) => {
-      if (err) return console.log(err)
-        console.log('saved to database');
-        res.end('added successfully');
-      })
-    })
-
-    app.post('/update', (req, res) => {
-      var id = req.body._id;
-      var user = req.body;
-      db.collection('user').update(
-        { '_id' : ObjectId(id) },
-        { $set: { 'fname': req.body.fname,
-        'lname': req.body.lname,
-        'email':req.body.email,
-        'password': req.body.password} },
-        function (err, result) {
-          if (err) throw err;
-        })
-        res.end('updated');
-    })
-
-    app.get('/delete',(req,res) => {
-      var url_parts = url.parse(req.url, true);
-      var query = url_parts.query;
-      var id = query._id;
-      var usertable = db.collection('user');
-      db.collection('user').deleteOne({_id: ObjectId(id)}, function (error, daigram){
-        res.end('deleted');
-      });
-    })
+// Start the server
+app.listen(port);
+console.log('Insert beer on port ' + port);
